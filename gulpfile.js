44ffd0cddfbe4a63b998692a47d1a1fd42a6b05d
Abhishek Jain <abhi2000@gmail.com>
2014-09-25 16:48:50 +0100

    Added gulp tasks for processing css

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 var gulp = require('gulp');
+var minify = require('gulp-minify-css');
 var $gulp = require('gulp-load-plugins')({
     lazy: false
 });
 
 gulp.task('css', function () {
-    gulp.src(['./app/styles/app.less'])
+    return gulp.src(['./app/styles/app.less'])
         .pipe($gulp.less())
+        .pipe(minify())
+        .pipe($gulp.rev())
         .pipe(gulp.dest('build/css/'));
 });
+
+gulp.task('clean', function () {
+    return gulp.src(['./build'], {read: false})
+        .pipe($gulp.clean());
+});
+
 
 gulp.task('html', ['css'], function () {
     return gulp.src('./app/index.html')
@@ -20,3 +29,12 @@ gulp.task('html', ['css'], function () {
         .pipe(gulp.dest('./build/'));
 });
 
+gulp.task('build', ['clean'], function() {
+    "use strict";
+    return gulp.start('css', 'html');
+});
+
+gulp.task('default', function() {
+    "use strict";
+   return gulp.start('build');
+});
