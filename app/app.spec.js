bd301722f8bbf752e5ea701378546e2ea9ab2e9c
jain208 <abhi2000@gmail.com>
2014-09-26 22:47:07 +0100

    Setup karma and a sample test

---
+++ app/app.spec.js
@@ -0,0 +1,8 @@
+/* globals describe, it, expect */
+
+describe("Sample test", function() {
+    "use strict";
+    it("should have 1 equal to 1", function() {
+        expect(1).toBe(1);
+    });
+});
