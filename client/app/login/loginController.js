c7fd31d574e0a03cf77d96e118a596f74e07dde0
Abhishek Jain <abhi2000@gmail.com>
2014-10-01 16:26:50 +0100

    login form structure and styling with basic validation - todo functionality

--- client/app/login/loginController.js
+++ client/app/login/loginController.js
@@ -2,6 +2,21 @@
     'use strict';
     app.controller('loginController', [function() {
         var vm = this;
-        vm.message = 'Hello from login controller';
+        initForm();
+        vm.submit = function(valid, data) {
+            if (!valid) {
+                return;
+            }
+
+            console.log(valid);
+            console.log(data);
+        };
+
+        function initForm() {
+            vm.user = {
+                email: '',
+                password: ''
+            };
+        }
     }]);
 })(angular.module('HousePointsApp'));
