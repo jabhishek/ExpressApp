305c3139c7e05fc004d3d0b33be3b2f9b89f9406
Abhishek Jain <abhi2000@gmail.com>
2014-10-01 14:07:33 +0100

    setup skeleton login page

--- client/app/app.js
+++ client/app/app.js
@@ -1,18 +1,22 @@
-
-(function() {
+(function () {
     'use strict';
     angular.module('HousePointsApp', ['ui.router'])
-        .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
-            $stateProvider.state('main', {
-                url: '/',
-                templateUrl: 'main/main.html',
-                controller: 'mainController as mainVm'
-            });
+        .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+            $stateProvider
+                .state('main', {
+                    url: '/',
+                    templateUrl: 'main/main.html',
+                    controller: 'mainController as mainVm'
+                })
+                .state('login', {
+                    url: '/login',
+                    templateUrl: 'login/login.html',
+                    controller: 'loginController as loginVm'
+                });
 
             $urlRouterProvider.otherwise('/');
 
             $locationProvider.html5Mode({enabled: true,
                 requireBase: false});
-
         });
 })();
---
+++ client/app/login/loginController.js
@@ -0,0 +1,7 @@
+(function (app) {
+    'use strict';
+    app.controller('loginController', [function() {
+        var vm = this;
+        vm.message = 'Hello from login controller';
+    }]);
+})(angular.module('HousePointsApp'));
