b6d177f4c8f04b18307f0fca0b6dadd0f936686e
Abhishek Jain <abhi2000@gmail.com>
2014-10-02 17:04:17 +0100

    setup local passport authentication

--- server/api/auth/local/index.js
+++ server/api/auth/local/index.js
@@ -1,14 +1,45 @@
 var express = require("express");
 var router = express.Router();
+var data = require("../../../data");
+var hasher = require("../hasher");
+var passport = require("passport");
+var localStrategy = require("passport-local").Strategy;
+
+function authenticateUser(email, password, next) {
+    "use strict";
+    data.users.getByEmail(email, function (err, user) {
+        if (!err && user) {
+            var hash = hasher.computeHash(password, user.salt);
+            if (hash === user.hashedPassword) {
+                next(null, user);
+                return;
+            }
+        }
+        next(null, false, {message: "Invalid credentials!!"});
+    });
+};
+
+passport.use(new localStrategy({
+        usernameField: 'email',
+        passwordField: 'password'
+    },
+    authenticateUser
+));
 
 // auth/local
 router.post('/', function (req, res, next) {
-    console.log("post");
     if (req.body.email && req.body.password) {
-        // todo-abhi passport validation
-        return res.json(200, {});
+        passport.authenticate('local', function (err, user, info) {
+            var error = err || info;
+            if (error) return res.json(401, error);
+            if (!user) return res.json(404, {message: 'Something went wrong, please try again.'});
+
+            // todo-abhi use jsonwebtoken to send token
+            res.json(200, {token: ""});
+        })(req, res, next)
+    } else {
+        return res.json(401, {});
     }
-    return res.json(401, {});
 });
 
 module.exports = router;
